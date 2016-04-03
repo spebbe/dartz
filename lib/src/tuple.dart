fbc1dfd32bcc57681bd6ff0eaf2afca7f2f82c05
Björn Sperber <spebbe@gmail.com>
2016-04-03 18:46:41 +0200

    Tuple semigroups and monoids are properly parameterized and no longer contain type cheats internally.

--- lib/src/tuple.dart
+++ lib/src/tuple.dart
@@ -11,25 +11,28 @@ class Tuple2<T1, T2> {
 
 Tuple2 tuple2(v1, v2) => new Tuple2(v1, v2);
 
-class Tuple2Semigroup extends Semigroup<Tuple2> {
-  final Semigroup _value1Semigroup;
-  final Semigroup _value2Semigroup;
+class Tuple2Semigroup<T1, T2> extends Semigroup<Tuple2<T1, T2>> {
+  final Semigroup<T1> _value1Semigroup;
+  final Semigroup<T2> _value2Semigroup;
 
   Tuple2Semigroup(this._value1Semigroup, this._value2Semigroup);
 
-  @override Tuple2 append(Tuple2 t1, Tuple2 t2) =>
-      new Tuple2(_value1Semigroup.append(t1.value1, t2.value1), _value2Semigroup.append(t1.value2, t2.value2));
+  @override Tuple2<T1, T2> append(Tuple2<T1, T2> t1, Tuple2<T1, T2> t2) =>
+      new Tuple2<T1, T2>(_value1Semigroup.append(t1.value1, t2.value1), _value2Semigroup.append(t1.value2, t2.value2));
 }
 
 Semigroup<Tuple2> tuple2Semigroup(Semigroup value1Semigroup, Semigroup value2Semigroup) => new Tuple2Semigroup(value1Semigroup, value2Semigroup);
 
-class Tuple2Monoid extends Tuple2Semigroup with Monoid<Tuple2> {
-  final Tuple2 _z;
+class Tuple2Monoid<T1, T2> extends Monoid<Tuple2<T1, T2>> {
+  final Monoid<T1> _value1Monoid;
+  final Monoid<T2> _value2Monoid;
 
-  Tuple2Monoid(Monoid v1m, Monoid v2m)
-      : _z = new Tuple2(v1m.zero(), v2m.zero()), super(v1m, v2m);
+  Tuple2Monoid(this._value1Monoid, this._value2Monoid);
 
-  @override Tuple2 zero() => _z;
+  @override Tuple2<T1, T2> append(Tuple2<T1, T2> t1, Tuple2<T1, T2> t2) =>
+      new Tuple2<T1, T2>(_value1Monoid.append(t1.value1, t2.value1), _value2Monoid.append(t1.value2, t2.value2));
+
+  @override Tuple2<T1, T2> zero() => new Tuple2<T1, T2>(_value1Monoid.zero(), _value2Monoid.zero());
 }
 
 Monoid<Tuple2> tuple2Monoid(Monoid value1Monoid, Monoid value2Monoid) => new Tuple2Monoid(value1Monoid, value2Monoid);
@@ -46,26 +49,30 @@ class Tuple3<T1, T2, T3> {
 
 Tuple3 tuple3(v1, v2, v3) => new Tuple3(v1, v2, v3);
 
-class Tuple3Semigroup extends Semigroup<Tuple3> {
-  final Semigroup _value1Semigroup;
-  final Semigroup _value2Semigroup;
-  final Semigroup _value3Semigroup;
+class Tuple3Semigroup<T1, T2, T3> extends Semigroup<Tuple3<T1, T2, T3>> {
+  final Semigroup<T1> _value1Semigroup;
+  final Semigroup<T2> _value2Semigroup;
+  final Semigroup<T3> _value3Semigroup;
 
   Tuple3Semigroup(this._value1Semigroup, this._value2Semigroup, this._value3Semigroup);
 
-  @override Tuple3 append(Tuple3 t1, Tuple3 t2) =>
-      new Tuple3(_value1Semigroup.append(t1.value1, t2.value1), _value2Semigroup.append(t1.value2, t2.value2), _value3Semigroup.append(t1.value3, t2.value3));
+  @override Tuple3<T1, T2, T3> append(Tuple3<T1, T2, T3> t1, Tuple3<T1, T2, T3> t2) =>
+      new Tuple3<T1, T2, T3>(_value1Semigroup.append(t1.value1, t2.value1), _value2Semigroup.append(t1.value2, t2.value2), _value3Semigroup.append(t1.value3, t2.value3));
 }
 
 Semigroup<Tuple3> tuple3Semigroup(Semigroup value1Semigroup, Semigroup value2Semigroup, Semigroup value3Semigroup) => new Tuple3Semigroup(value1Semigroup, value2Semigroup, value3Semigroup);
 
-class Tuple3Monoid extends Tuple3Semigroup with Monoid<Tuple3> {
-  final Tuple3 _z;
+class Tuple3Monoid<T1, T2, T3> extends Monoid<Tuple3<T1, T2, T3>> {
+  final Monoid<T1> _value1Monoid;
+  final Monoid<T2> _value2Monoid;
+  final Monoid<T3> _value3Monoid;
 
-  Tuple3Monoid(Monoid v1m, Monoid v2m, Monoid v3m)
-      : _z = new Tuple3(v1m.zero(), v2m.zero(), v3m.zero()), super(v1m, v2m, v3m);
+  Tuple3Monoid(this._value1Monoid, this._value2Monoid, this._value3Monoid);
 
-  @override Tuple3 zero() => _z;
+  @override Tuple3<T1, T2, T3> append(Tuple3<T1, T2, T3> t1, Tuple3<T1, T2, T3> t2) =>
+      new Tuple3<T1, T2, T3>(_value1Monoid.append(t1.value1, t2.value1), _value2Monoid.append(t1.value2, t2.value2), _value3Monoid.append(t1.value3, t2.value3));
+
+  @override Tuple3<T1, T2, T3> zero() => new Tuple3<T1, T2, T3>(_value1Monoid.zero(), _value2Monoid.zero(), _value3Monoid.zero());
 }
 
 Monoid<Tuple3> tuple3Monoid(Monoid value1Monoid, Monoid value2Monoid, Monoid value3Monoid) => new Tuple3Monoid(value1Monoid, value2Monoid, value3Monoid);
@@ -84,28 +91,33 @@ class Tuple4<T1, T2, T3, T4> {
 
 Tuple4 tuple4(v1, v2, v3, v4) => new Tuple4(v1, v2, v3, v4);
 
-class Tuple4Semigroup extends Semigroup<Tuple4> {
-  final Semigroup _value1Semigroup;
-  final Semigroup _value2Semigroup;
-  final Semigroup _value3Semigroup;
-  final Semigroup _value4Semigroup;
+class Tuple4Semigroup<T1, T2, T3, T4> extends Semigroup<Tuple4<T1, T2, T3, T4>> {
+  final Semigroup<T1> _value1Semigroup;
+  final Semigroup<T2> _value2Semigroup;
+  final Semigroup<T3> _value3Semigroup;
+  final Semigroup<T4> _value4Semigroup;
 
   Tuple4Semigroup(this._value1Semigroup, this._value2Semigroup, this._value3Semigroup, this._value4Semigroup);
 
-  @override Tuple4 append(Tuple4 t1, Tuple4 t2) =>
-      new Tuple4(_value1Semigroup.append(t1.value1, t2.value1), _value2Semigroup.append(t1.value2, t2.value2), _value3Semigroup.append(t1.value3, t2.value3), _value4Semigroup.append(t1.value4, t2.value4));
+  @override Tuple4<T1, T2, T3, T4> append(Tuple4<T1, T2, T3, T4> t1, Tuple4<T1, T2, T3, T4> t2) =>
+      new Tuple4<T1, T2, T3, T4>(_value1Semigroup.append(t1.value1, t2.value1), _value2Semigroup.append(t1.value2, t2.value2), _value3Semigroup.append(t1.value3, t2.value3), _value4Semigroup.append(t1.value4, t2.value4));
 }
 
 Semigroup<Tuple4> tuple4Semigroup(Semigroup value1Semigroup, Semigroup value2Semigroup, Semigroup value3Semigroup, Semigroup value4Semigroup) => new Tuple4Semigroup(value1Semigroup, value2Semigroup, value3Semigroup, value4Semigroup);
 
 
-class Tuple4Monoid extends Tuple4Semigroup with Monoid<Tuple4> {
-  final Tuple4 _z;
+class Tuple4Monoid<T1, T2, T3, T4> extends Monoid<Tuple4<T1, T2, T3, T4>> {
+  final Monoid<T1> _value1Monoid;
+  final Monoid<T2> _value2Monoid;
+  final Monoid<T3> _value3Monoid;
+  final Monoid<T4> _value4Monoid;
 
-  Tuple4Monoid(Monoid v1m, Monoid v2m, Monoid v3m, Monoid v4m)
-      : _z = new Tuple4(v1m.zero(), v2m.zero(), v3m.zero(), v4m.zero()), super(v1m, v2m, v3m, v4m);
+  Tuple4Monoid(this._value1Monoid, this._value2Monoid, this._value3Monoid, this._value4Monoid);
 
-  @override Tuple4 zero() => _z;
+  @override Tuple4<T1, T2, T3, T4> append(Tuple4<T1, T2, T3, T4> t1, Tuple4<T1, T2, T3, T4> t2) =>
+      new Tuple4<T1, T2, T3, T4>(_value1Monoid.append(t1.value1, t2.value1), _value2Monoid.append(t1.value2, t2.value2), _value3Monoid.append(t1.value3, t2.value3), _value4Monoid.append(t1.value4, t2.value4));
+
+  @override Tuple4<T1, T2, T3, T4> zero() => new Tuple4<T1, T2, T3, T4>(_value1Monoid.zero(), _value2Monoid.zero(), _value3Monoid.zero(), _value4Monoid.zero());
 }
 
 Monoid<Tuple4> tuple4Monoid(Monoid value1Monoid, Monoid value2Monoid, Monoid value3Monoid, Monoid value4Monoid) => new Tuple4Monoid(value1Monoid, value2Monoid, value3Monoid, value4Monoid);

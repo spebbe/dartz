6903d0114db0c2e26a76b122eebb90b5877b878a
Björn Sperber <spebbe@gmail.com>
2016-05-11 22:02:50 +0200

    Id type parameterization

--- lib/src/id.dart
+++ lib/src/id.dart
@@ -3,9 +3,11 @@ part of dartz;
 class IdMonad extends Monad {
   @override pure(a) => a;
   @override bind(fa, f(_)) => f(fa);
+
+  IList/*<A>*/ replicate/*<A>*/(int n, /*=A*/ fa) => super.replicate(n, fa) as IList/*<A>*/;
 }
 
-final Monad IdM = new IdMonad();
+final IdMonad IdM = new IdMonad();
 
 class IdTraversable extends Traversable {
   @override /*=G*/ traverse/*<G>*/(Applicative/*<G>*/ gApplicative, fa, /*=G*/ f(a)) => f(fa);

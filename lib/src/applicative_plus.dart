b1781498bfee34abf1d2f6eac99a8806688f4422
Björn Sperber <spebbe@gmail.com>
2016-07-03 18:16:13 +0200

    prependElement and appendElement operations for ApplicativePlus

--- lib/src/applicative_plus.dart
+++ lib/src/applicative_plus.dart
@@ -1,7 +1,11 @@
 part of dartz;
 
 abstract class ApplicativePlus<F> extends Applicative<F> implements PlusEmpty<F> {
+  F prependElement/*<A>*/(F fa, /*=A*/ a) => plus(pure(a), fa);
+  F appendElement/*<A>*/(F fa, /*=A*/ a) => plus(fa, pure(a));
 }
 
 abstract class ApplicativePlusOps<F, A> extends ApplicativeOps<F, A> with PlusEmptyOps<F, A> {
+  F prependElement(A a) => (pure(a) as dynamic/*=ApplicativePlusOps<F, A>*/).plus(this as dynamic/*=F*/);
+  F appendElement(A a) => plus(pure(a));
 }

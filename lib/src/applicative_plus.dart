c16f9a78f2c8e32ebaafc94872b8d3d2f3726b8f
Björn Sperber <spebbe@gmail.com>
2016-07-03 20:57:16 +0200

    Fixed incompatibilities with dart2js

--- lib/src/applicative_plus.dart
+++ lib/src/applicative_plus.dart
@@ -1,11 +1,11 @@
 part of dartz;
 
-abstract class ApplicativePlus<F> extends Applicative<F> implements PlusEmpty<F> {
+abstract class ApplicativePlus<F> implements Applicative<F>, PlusEmpty<F> {
   F prependElement/*<A>*/(F fa, /*=A*/ a) => plus(pure(a), fa);
   F appendElement/*<A>*/(F fa, /*=A*/ a) => plus(fa, pure(a));
 }
 
-abstract class ApplicativePlusOps<F, A> extends ApplicativeOps<F, A> with PlusEmptyOps<F, A> {
+abstract class ApplicativePlusOps<F, A> implements ApplicativeOps<F, A>, PlusEmptyOps<F, A> {
   F prependElement(A a) => (pure(a) as dynamic/*=ApplicativePlusOps<F, A>*/).plus(this as dynamic/*=F*/);
   F appendElement(A a) => plus(pure(a));
 }

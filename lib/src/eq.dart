cdc4809073db39c6be6f678cb60dec0bf06a2af6
Björn Sperber <spebbe@gmail.com>
2016-08-20 22:07:55 +0200

    Added helpers for creating anonymous Eq instances as well as an Eq instance for comparing Objects using '=='

--- lib/src/eq.dart
+++ lib/src/eq.dart
@@ -9,3 +9,14 @@ abstract class EqOps<A> {
   bool eq(A other);
   bool neq(A other) => !eq(other);
 }
+
+typedef bool EqF<A>(A a1, A a2);
+class _AnonymousEq<A> extends Eq<A> {
+  final EqF _f;
+  _AnonymousEq(this._f);
+  @override bool eq(A a1, A a2) => _f(a1, a2);
+}
+
+Eq/*<A>*/ eq/*<A>*/(EqF/*<A>*/ f) => new _AnonymousEq(f);
+
+final Eq<Object> ObjectEq = eq((o1, o2) => o1 == o2);

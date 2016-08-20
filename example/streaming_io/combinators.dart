46f13525a5b153671c0d12a3416cfb1066aeb9cb
Björn Sperber <spebbe@gmail.com>
2016-08-20 22:10:39 +0200

    Funner, faster and longer streaming IO example

--- example/streaming_io/combinators.dart
+++ example/streaming_io/combinators.dart
@@ -5,22 +5,41 @@ import 'dart:io';
 import 'dart:async';
 import '../free_io/io.dart';
 import '../free_io/console_io.dart';
+import 'dart:convert';
 
 Future<Either<Object, IList/*<A>*/>> dump/*<A>*/(Conveyor<Free<IOOp, dynamic>, String> conveyor) =>
     unsafePerformIO/*<Either<Object, IList<A>>>*/(IOM.attempt(conveyor.sink(println).runLog(IOM)));
 
-// Note: Just a toy implementation!!! RIDICULOUSLY inefficient and doesn't work for multi-byte characters!
+// Note: Just a basic implementation!!! Assumes Latin-1 encoding, not super efficient, naively buffers extremely long lines, etc...
 Conveyor<Free<IOOp, dynamic>, String> fileLines(String path) => Source.resource(
     println("\n*** opening $path ***") >> openFile(path),
     (RandomAccessFile file) =>
-        Source.eval(readBytes(file, 1))
+        Source.eval(readBytes(file, 4096))
             .repeat()
             .takeWhile((byte) => byte.isNotEmpty)
-            .map(SYSTEM_ENCODING.decode)
-            .pipe(bufferLine.repeat()),
+            .map(LATIN1.decode)
+            .pipe(bufferLines),
     (RandomAccessFile file) =>
         Source.eval_(println("*** closing $path ***") >> closeFile(file)));
 
-Conveyor<From<String>, String> bufferLine = Pipe.takeWhile((String s) => s != "\n").concatenate(StringMi);
+Conveyor<From<String>, String> _bufferLines(Option<String> spill) =>
+    Pipe.consume((s) {
+      final buffered = (spill|"") + s;
+      final lines = ilist(buffered.split("\n"));
+      return lines.reverse().uncons(Pipe.halt, (newSpill, completeLines) =>
+          completeLines.foldLeft/*<Conveyor<From<String>, String>>*/(_bufferLines(some(newSpill)), (rest, line) => Conveyor.produce(line, rest))
+      );
+    }, () => spill.fold(Pipe.halt, Pipe.produce));
+
+Conveyor<From<String>, String> bufferLines = _bufferLines(none()).repeat();
 
 final toUppercase = Pipe.lift((String s) => s.toUpperCase());
+
+Conveyor<From/*<A>*/, dynamic/*=A*/> skipDuplicates/*<A>*/([Eq/*<A>*/ _eq]) {
+  final Eq/*<A>*/ eq = _eq ?? ObjectEq;
+  Conveyor<From/*<A>*/, dynamic/*=A*/> loop(Option/*<A>*/ last) =>
+      Pipe.consume((/*=A*/ a) => last.map((lastA) => eq.eq(lastA, a)) | false
+          ? loop(last)
+          : Pipe.produce(a, loop(some(a))));
+  return loop(none());
+}
--- example/streaming_io/example.dart
+++ example/streaming_io/example.dart
@@ -10,14 +10,16 @@ main() async {
   // Construct Conveyor that:
   // 1. Reads lines from this file
   // 2. Trims leading and trailing whitespace
-  // 3. Finds single line comments
-  // 4. Stops after five comments are consumed
+  // 3, 4. Finds single line comments and removes consecutive duplicates
+  // 3, 4. Finds single line comments and removes consecutive duplicates
+  // 5. Stops after five comments are consumed
   final firstFiveCommentsInThisFile = fileLines(pathToThisFile)
       .map((line) => line.trim())
       .filter((line) => line.startsWith("//"))
+      .pipe(skipDuplicates())
       .take(5);
 
-  // This is the sixth comment. It won't ever be read from the file,
+  // This is the sixth comment. It might never be read from the file,
   // since the Conveyor finishes as soon as the fifth comment is consumed.
   // The file will be closed as soon as the Conveyor finishes.
 

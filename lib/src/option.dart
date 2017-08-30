7d846a69868231da34266d3eacb0b39f22fa6bf7
Björn Sperber <bjorn.sperber@soundtrap.com>
2017-08-30 22:22:13 +0200

    Added optionOf for creating Option from possibly null values

--- lib/src/option.dart
+++ lib/src/option.dart
@@ -48,6 +48,7 @@ final Option _none = new None();
 Option<A> none<A>() => cast(_none);
 Option<A> some<A>(A a) => new Some(a);
 Option<A> option<A>(bool test, A value) => test ? some(value) : none();
+Option<A> optionOf<A>(A value) => value != null ? some(value) : none();
 
 class OptionMonadPlus extends MonadPlusOpsMonadPlus<Option> {
   OptionMonadPlus() : super(some, none);

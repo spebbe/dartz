c95ac0af75de7fb1671b0e29b15904d3723cce27
Björn Sperber <bjorn.sperber@soundtrap.com>
2019-06-17 18:41:58 +0200

    Workaround for https://github.com/dart-lang/sdk/issues/35097 (closes #12 and #16)

--- lib/src/endo.dart
+++ lib/src/endo.dart
@@ -2,14 +2,8 @@ part of dartz;
 
 typedef A Endo<A>(A a);
 
-class EndoMonoid<A> extends Monoid<Endo<A>> {
-  @override Endo<A> zero() => (A a) => a;
-
-  @override Endo<A> append(Endo<A> e1, Endo<A> e2) => (A a) => e1(e2(a));
-}
-
-final Monoid<Endo> EndoMi = new EndoMonoid();
-Monoid<Endo<A>> endoMi<A>() => new EndoMonoid();
+Monoid<Endo<A>> endoMi<A>() => monoid(() => (A a) => a, (Endo<A> e1, Endo<A> e2) => (A a) => e1(e2(a)));
+final Monoid<Endo> EndoMi = endoMi();
 
 final Monoid<Endo> DualEndoMi = dualMonoid(EndoMi);
 Monoid<Endo<A>> dualEndoMi<A>() => dualMonoid(endoMi());
